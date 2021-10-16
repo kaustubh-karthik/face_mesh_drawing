010cfd6c5a66292b319261ec1b36df7d8b5b798e
Kaustubh Karthik <kaustubhkarthik@gmail.com>
2021-10-16 21:08:43 +0100

    Updated display_img to include fps

--- face_mesh_utils.py
+++ face_mesh_utils.py
@@ -68,7 +68,7 @@ class detector():
         if fps:
             curr_time = time.time()
             fps = 1/(curr_time - self.prev_time)
-            prev_time = curr_time
+            self.prev_time = curr_time
 
 
         cv2.putText(
